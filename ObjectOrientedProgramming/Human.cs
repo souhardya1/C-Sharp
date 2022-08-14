038febfc73d44b7a82a638e26dcb316ae28a56c5
Souhardya Chakraborty <64438285+souhardya1@users.noreply.github.com>
2022-08-14 13:11:24 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/Human.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    // class is a blueprint
+    internal class Human
+    {
+        //member variable
+        private string firstName="default first name";
+        private string lastName="default last name";
+        private string eyeColor="default Colour";
+        private int age;
+
+
+        // default constructor
+        public Human()
+        {
+            Console.WriteLine("Inside defualt constructor");
+        }
+
+        // 1 parameter constructor
+        public Human(string myfirstName)
+        {
+            Console.WriteLine("Inside 1 parameter constructor.");
+            firstName = myfirstName;
+        }
+
+        // 2 parameter constructor
+        public Human(string myfirstName, string lastName)
+        {
+            Console.WriteLine("Inside 2 parameter constructor.");
+            firstName = myfirstName;
+            this.lastName = lastName; //this key refers to global variable
+   
+        }
+
+        // 3 parameter construtor
+        public Human(string myfirstName, string lastName, string eyeColor)
+        {
+            Console.WriteLine("Inside 3 parameter constructor.");
+            firstName = myfirstName;
+            this.lastName = lastName; //this key refers to global variable
+            this.eyeColor = eyeColor;
+        }
+
+        // 3 parameter construcor with age
+        public Human(string myfirstName, string lastName, int age)
+        {
+            Console.WriteLine("Inside 3 parameter constructor with age.");
+            firstName = myfirstName;
+            this.lastName = lastName; //this key refers to global variable
+            this.age = age;
+        }
+        // 4 parameter constructor
+        public Human(string myfirstName,string lastName,string eyeColor, int age)
+        {
+            Console.WriteLine("Inside 4 parameter constructor.");
+            firstName=myfirstName;
+            this.lastName = lastName; //this key refers to global variable
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        // member
+        public void introduce()
+        {
+            Console.WriteLine("Hi I am {0} {1} and {3} years old and my eye color is {2}",firstName,lastName,eyeColor,age);
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ObjectOrientedProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of my class
+            // an instance of Human
+            Human me = new Human("Souhardya","Charaborty","black",22);
+
+            // access public variable from outside and even change it
+            /*me.firstName = "Souhardya";*/
+
+            //call methods of class
+            me.introduce();
+
+            Human you = new Human("Purbasha", "Ghosh", 21);// calls constructor with 3parameter and age
+            you.introduce();
+
+
+            Human friend1 = new Human(); // calls default constructor
+            friend1.introduce();
+
+            Human friend2 =  new Human("Bubu","Sordar","blue"); // calls constructor with 3parameter and eyecolor 
+            friend2.introduce();
+
+
+
+
+        }
+    }
+}
