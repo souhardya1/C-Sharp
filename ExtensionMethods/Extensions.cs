e7d595c2160162407c83be899a4d467590b7c23b
souhardya1 <kittu.chakrovartee@gmail.com>
2022-11-12 12:08:03 +0530

    Extensions

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal static class Extensions
+    {
+        public static void BubbleSort(this int[] arr, bool reverse = false)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            if (reverse)
+            {
+                Array.Reverse(arr);
+            }
+        }
+
+        public static void Reverse (this int[] array)
+        {
+             Array.Reverse(array);
+        }
+
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExtensionMethods
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 3, 6, 7, 2, 9, 1, 2, 6, 2 };
+            string[] strArr = { "asd", "Aseq", "qwe", "popo" };
+
+            arr.BubbleSort();
+            Console.WriteLine("Only Sort: " + string.Join(",", arr));
+
+            arr.BubbleSort(true);
+            Console.WriteLine("Sort and Reverse: " + string.Join(",", arr));
+
+            arr.Reverse();
+            Console.WriteLine("Only reverse: " + string.Join(",", arr));
+
+         /* 
+             Because of the this keyword(in Extensions.cs) we can use arr.BubbleSort() not using BubbleSort(arr). This is a Extension.
+             We cant use this on any type of array( e.g str[] ) other than int[] as the parameter is this int[]. So, strArr.BubbleSort(); not possible reason mentioned below
+        */
+        }
+    }
+}
