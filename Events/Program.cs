c45a6ec7f272e2d6456586d1b60efd5c6e5961e5
souhardya1 <kittu.chakrovartee@gmail.com>
2022-11-13 17:26:43 +0530

    Events

---
+++ Events/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Shooter shooter = new Shooter();
+            shooter.ShotsFired += KilledEnemy;
+            shooter.OnShoot();
+        }
+
+        public static void KilledEnemy(object sender,EventArgs e)
+        {
+            Console.WriteLine("I killed an enemy");
+        }
+        
+    }
+    internal class Shooter
+    {
+        private Random rng = new Random();
+        public delegate void KillingHandler(object sender, EventArgs e);
+        public event KillingHandler? ShotsFired;
+
+        public void OnShoot()
+        {
+            while (true)
+            {
+                if (rng.Next(0, 100) % 2 == 0)
+                {
+                    if(ShotsFired != null)
+                    {
+                        ShotsFired.Invoke(this,EventArgs.Empty);
+                    }                 
+                }
+                else
+                {
+                    Console.WriteLine("I missed");
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
